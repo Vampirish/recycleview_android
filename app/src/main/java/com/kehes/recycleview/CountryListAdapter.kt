273b330a7204ae5066248098af901453e25f4f00
Vampirish <vamperish@gmail.com>
2024-10-06 17:02:29 +0200

    CountryList added

---
+++ app/src/main/java/com/kehes/recycleview/CountryListAdapter.kt
@@ -0,0 +1,31 @@
+package com.kehes.recycleview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kehes.recycleview.databinding.ItemCountryBinding
+
+class CountryListAdapter(private val items: List<String>):
+    RecyclerView.Adapter<CountryListAdapter.CountryViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
+        return CountryViewHolder(
+            ItemCountryBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+        holder.bindView(items[position])
+    }
+
+    class CountryViewHolder(private val binding: ItemCountryBinding):
+        RecyclerView.ViewHolder(binding.root) {
+        fun bindView(item: String) {
+            binding.root.text = item
+        }
+    }
+}
